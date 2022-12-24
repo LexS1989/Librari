b33dd2772440cf135a37976dbdce68a6a940aa84
Alexey <lexshadrin@mail.ru>
2022-12-24 20:32:03 +1100

    Initial commit

---
+++ src/main/java/ru/skypro/librari/LibrariApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.librari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrariApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrariApplication.class, args);
+    }
+
+}
